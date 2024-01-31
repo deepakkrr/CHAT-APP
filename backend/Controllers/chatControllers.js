a326901de49a67bd62fc6e69c354d4b1b0bb767e
deepakkr <deepaksinghofficial2224@gmail.com>
2024-01-31 23:37:10 +0530

    c

--- backend/Controllers/chatControllers.js
+++ backend/Controllers/chatControllers.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const Chat = require("../models/chatModel");
+const Chat = require("../Models/chatModel");
 const User = require("../Models/userModel");
 
 //@description     Create or fetch One to One Chat
